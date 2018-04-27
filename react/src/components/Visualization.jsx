e643a0c6c821246eab42c2116fc6a263b19400b6
Kurtis <kurtis.david@live.com>
2018-04-27 14:50:38 -0500

    added some mounting, need to delay DL

--- react/src/components/Visualization.jsx
+++ react/src/components/Visualization.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+// import us_states from './us-states.js'
 import {geoPath} from "d3-geo";
 import { select } from 'd3-selection';
 // import {albersUsa} from "d3-geo-projection";
@@ -7,6 +8,28 @@ class Visualization extends Component{
 
 	constructor(props){
 		super(props)
+		this.state = {
+	      parks:[],
+	      us_states:[]
+	    }
+	}
+
+	componentWillMount(){
+		var url = "https://raw.githubusercontent.com/iambrandonchan/learning2earn/master/react/src/components/state_parks.csv";
+		var data = [];
+		d3.csv(url, function(d) {
+			d['num_parks'] = parseInt(d['num_parks']);
+			data.push(d);
+		});
+
+		var url_j = "https://raw.githubusercontent.com/iambrandonchan/learning2earn/master/react/src/components/us-states.json";
+		var lmao = [];
+		d3.json(url_j, function(error,json){
+			console.log(typeof json);
+			lmao = json[0].features;
+		});
+
+		this.setState({parks:data, us_states:lmao});
 	}
 
 	render(){
@@ -26,32 +49,24 @@ class Visualization extends Component{
 		    .attr("width", width)
 		    .attr("height", height);
 
-		// svg.append("rect")
-		//     .attr("class", "background")
-		//     .attr("width", width)
-		//     .attr("height", height);
+		svg.append("rect")
+		    .attr("class", "background")
+		    .attr("width", width)
+		    .attr("height", height);
 		    // .on("click", click);
-		var url = "https://raw.githubusercontent.com/iambrandonchan/learning2earn/master/react/src/components/state_parks.csv";
-		var data = [];
-		d3.csv(url, function(d) {
-			d['num_parks'] = parseInt(d['num_parks']);
-			data.push(d);
-		});
+		
+		console.log(this.state['us_states'])
 
-		// d3.json("https://bubinga.co/wp-content/uploads/jsonstates.min_.js", function(json){
-		// 	console.log(json.features);
-		// });
-
-		svg.selectAll("path")
-			.data(data)
-			.enter()
-			.append("path")
-			.attr("d", path)
-			.style("stroke", "#fff")
-			.style("stroke-width", "1")
-			.style("fill", function(d) {
-				return "rgb(213,222,217)";
-			});
+		// svg.selectAll("path")
+		// 	.data(data)
+		// 	.enter()
+		// 	.append("path")
+		// 	.attr("d", path)
+		// 	.style("stroke", "#fff")
+		// 	.style("stroke-width", "1")
+		// 	.style("fill", function(d) {
+		// 		return "rgb(213,222,217)";
+		// 	});
 
 	 	return(
         <p>LOL</p>
